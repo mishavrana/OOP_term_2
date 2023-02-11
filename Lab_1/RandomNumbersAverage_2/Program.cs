f19827f7b7adec772faadd561ed7bf3eadb7d3c9
Mykhailo <misha.vrana@gmail.com>
2023-02-12 00:23:16 +0200

    RandomNumbersAverage_2 added

---
+++ Lab_1/RandomNumbersAverage_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RandomNumbersAverage_2
+{
+    public delegate double Average(int x, int y, int z);
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(average(2, 4, 7));
+        }
+
+        private static Average average = delegate(int x, int y, int z)
+        {
+            return Math.Round(Convert.ToDouble(x + y + z) / 3, 2);
+        };
+    }
+}

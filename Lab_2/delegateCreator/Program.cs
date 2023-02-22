98a13c73d7be61fda5b1900b29c5adae500faab1
Mykhailo <misha.vrana@gmail.com>
2023-02-22 23:38:18 +0200

    delegateCreator added

---
+++ Lab_2/delegateCreator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab2.DelegateCreator;
+
+public delegate int StringCount(string row);
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating a delegate with lambda expression
+        StringCount operation = row => row.Length;
+        
+        // Output information 
+        Console.WriteLine(operation("success"));
+    }
+}
